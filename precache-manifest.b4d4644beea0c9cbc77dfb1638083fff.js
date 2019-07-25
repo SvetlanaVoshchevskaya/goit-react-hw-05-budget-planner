477cdbeb4d3857d1462bc4c26270ee8ddce5711a
Svetlana Voshchevskaya <VSveta.83@gmail.com>
2019-07-25 13:20:20 +0300

    Updates

---
+++ precache-manifest.b4d4644beea0c9cbc77dfb1638083fff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44987e9805eeb8992a3cd857a318b4e",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "b80fb297d87b355bb6a9",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "80baad9348abb5a73777",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.3caf0e56.chunk.js"
+  },
+  {
+    "revision": "b80fb297d87b355bb6a9",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.56393207.chunk.js"
+  },
+  {
+    "revision": "549cb78f4abd9d607011",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0cda3cff.js"
+  }
+]);
